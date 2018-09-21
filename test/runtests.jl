5a134ca977228ac6f53646d4c71049af6415c6dd
roger <roger.herikstad@gmail.com>
2018-09-21 17:12:50 +0800

    Adds test for town evolution

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,6 @@
 using Merkleville
+using Random
 using Test
-
 
 @testset "Neighbours" begin
     A = [[-1 1];[1 -1]]
@@ -10,5 +10,13 @@ using Test
     @test n2 == 1
     n3 = Merkleville.get_neighbour(A, 1,2, 0,1)
     @test n3 == -1
+end
+
+@testset "Small town" begin
+    RNG = MersenneTwister(1234)
+    town = Merkleville.rand(RNG, Merkleville.Town, 5,5)
+    @test town.A == [-1 -1 1 -1 -1; 1 -1 1 1 1; 1 1 -1 -1 -1; 1 -1 -1 -1 1; -1 1 1 1 1]
+    Merkleville.step!(town, 4)
+    @test town.A == [-1 -1 1 1 1; 1 -1 1 1 1; 1 -1 -1 -1 -1; 1 1 -1 -1 -1; -1 1 1 -1 1]
 end
 
